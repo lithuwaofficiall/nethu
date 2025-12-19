d14a027f9a5d13917eb827113a37be00432b7222
Lithuwaofficial <gaminglithu8@gmail.com>
2025-12-19 15:31:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "lithu=P4wwEBZL#ZHaTlV1dPp_w85femow4BcuVBA8YfLBbs4aJKUMgDO8", // ඔයාගේ session id එක දාන්න
-MONGODB: process.env.MONGODB || "mongodb+srv://sulabijja:sulabijja@demon.d4ov0.mongodb.net/?retryWrites=true&w=majority&appName=DEMON", // ඔයාගේ mongodb url එක දාන්න
+MONGODB: process.env.MONGODB || "mongodb://mongo:jUZBPbznsHkbJWTDmevvEFdrpGCxcEpa@metro.proxy.rlwy.net:46411", // ඔයාගේ mongodb url එක දාන්න
 LANG: process.env.LANG || "EN",
 BUTTON: process.env.BUTTON || "true",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
